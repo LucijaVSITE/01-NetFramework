efacd8745bc013a4ae3e883693f4a8e66fe46aa6
Lucija Ivanković VSITE2015 <luivanko@vsite.hr>
2016-10-13 20:16:24 +0200

    Dovršili smo .NetFramework

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -5,8 +5,14 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)
+
         {
+            int a = 14;
+            double d = 3.14;
+            Console.WriteLine(a);
+            Console.WriteLine(d);
             Console.WriteLine("Zdravo");
+            Console.ReadKey();
         }
     }
 }
